d9209daf964f055b4ca26a651518be9559dedd7e
jt <jiaan.t@gmail.com>
2017-08-05 04:14:12 -0400

    fixed current time

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,14 @@
 
 $(document).ready(function () {
+  var today = new Date();
+  var d = today.getDate();
+  var m = today.getMonth() + 1; //January is 0!
+  var y = today.getFullYear();
   console.log("test");
-  $.get("/8%204,%202012", function (data) {
-    var html ='';
-    html += '<p>Natural: ' + data.Date +'</p>';
-    html += '<p>Unix: ' +data.Unix +'</p>';
+  $.get("/"+m+"%20"+d+",%20"+y, function (data) {
+    var html = '';
+    html += '<p>Natural: ' + data.Natural + '</p>';
+    html += '<p>Unix: ' + data.Unix + '</p>';
     $(".lead").html(html);
     console.log("Foo function:", data.Unix);
   });
